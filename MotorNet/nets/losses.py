f5c29252e55c1bc995a2704e77de5203e2048eba
Olivier <codol.olivier@gmail.com>
2021-11-15 21:47:50 -0500

    final merge rollback

--- MotorNet/nets/losses.py
+++ MotorNet/nets/losses.py
@@ -3,7 +3,6 @@ from tensorflow.python.keras.utils import losses_utils
 from tensorflow.python.keras.losses import LossFunctionWrapper
 
 
-
 class L2Regularizer(LossFunctionWrapper):
     def __init__(self, name='l2_regularizer', reduction=losses_utils.ReductionV2.AUTO):
         super().__init__(y_pred_l2, name=name, reduction=reduction)
@@ -66,60 +65,3 @@ def scale_activation(activation, max_iso_force):
 
 def y_pred_l2(y_true, y_pred):
     return tf.sqrt(y_pred ** 2)
-
-def empty_loss():
-    def loss(y_true, y_pred):
-        return tf.reduce_sum(tf.zeros_like(y_true))
-    return loss
-
-
-def position_loss():
-    def loss(y_true, y_pred):
-        true_pos, _ = tf.split(y_true, 2, axis=-1)
-        pred_pos, _ = tf.split(y_pred, 2, axis=-1)
-        return tf.reduce_mean(tf.abs(true_pos - pred_pos))
-    return loss
-
-def position_loss_bis():
-    def loss(y_true, y_pred):
-        true_pos,_ = tf.split(y_true, 2, axis=-1)
-        pred_pos,_ = tf.split(y_pred, 2, axis=-1)
-        return tf.reduce_mean(tf.norm(true_pos - pred_pos))
-    return loss
-
-def activation_squared_loss(max_iso_force):
-    def loss(y_true, y_pred):
-        activations = tf.slice(y_pred, [0, 0, 0, 0], [-1, -1, 1, -1])
-        # normalize max muscle force
-        max_iso_force_norm = max_iso_force / tf.reduce_mean(max_iso_force)
-        # scale activation penalty by maximum muscle force of each muscle
-        activations_scaled = activations * tf.tile(tf.expand_dims(max_iso_force_norm, axis=2),
-                                                   (tf.shape(activations)[0], tf.shape(activations)[1], 1, 1))
-        return tf.reduce_mean(activations_scaled ** 2)
-    return loss
-
-
-def activation_velocity_squared_loss(max_iso_force, vel_weight=0.):
-    def loss(y_true, y_pred):
-        activations = tf.slice(y_pred, [0, 0, 0, 0], [-1, -1, 1, -1])
-        muscle_vel = tf.slice(y_pred, [0, 0, 2, 0], [-1, -1, 1, -1])
-        # normalize max muscle force
-        max_iso_force_norm = max_iso_force / tf.reduce_mean(max_iso_force)
-        # scale activation penalty by maximum muscle force of each muscle
-        activations_scaled = activations * tf.tile(tf.expand_dims(max_iso_force_norm, axis=2),
-                                                   (tf.shape(activations)[0], tf.shape(activations)[1], 1, 1))
-        return tf.reduce_mean(activations_scaled ** 2) + vel_weight*tf.reduce_mean(tf.abs(muscle_vel))
-    return loss
-
-
-def activation_diff_squared_loss(max_iso_force, vel_weight=0.):
-    def loss(y_true, y_pred):
-        activations = tf.slice(y_pred, [0, 0, 0, 0], [-1, -1, 1, -1])
-        # normalize max muscle force
-        max_iso_force_norm = max_iso_force / tf.reduce_mean(max_iso_force)
-        # scale activation penalty by maximum muscle force of each muscle
-        activations_scaled = activations * tf.tile(tf.expand_dims(max_iso_force_norm, axis=2),
-                                                   (tf.shape(activations)[0], tf.shape(activations)[1], 1, 1))
-        activation_vel = tf.reduce_mean(tf.math.subtract(activations_scaled[1:], activations_scaled[0:-1])**2)*100
-        return tf.reduce_mean(activations_scaled ** 2) + vel_weight*activation_vel
-    return loss
