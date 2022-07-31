7b16df941a90301674e8b1913ab1cb0d3215e356
Olivier <codol.olivier@gmail.com>
2022-07-31 17:19:20 -0400

    updated version tag

--- motornet/__init__.py
+++ motornet/__init__.py
@@ -3,7 +3,7 @@ A TensorFlow-powered python toolbox to train deep neural networks to perform mot
 """
 
 __name__ = "motornet"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = 'Olivier Codol'
 
 from . import nets
