b2d3fe1500a5854810dd7a743d18c411daef4ec4
Olivier Codol <codol.olivier@gmail.com>
2023-07-13 11:48:54 -0400

    updated requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import setup
-import platform
-
-system = platform.system().lower()
-processor = platform.machine()
-tensorflow_version = "tensorflow-macos" if system == "darwin" and processor == "arm64" else "tensorflow"
 
 install_req = [
-    tensorflow_version,
     "numpy",
-    "matplotlib",
-    "ipython",
-    "joblib",
+    "torch",
+    "gymnasium",
 ]
 
 if __name__ == "__main__":
