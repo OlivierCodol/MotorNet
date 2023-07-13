8d961ab7b34974d78ec7c91808d6127e2944f2ae
Olivier Codol <codol.olivier@gmail.com>
2023-07-13 15:53:44 -0400

    reverted version single-sourcing from .toml to __init__

--- motornet/__init__.py
+++ motornet/__init__.py
@@ -2,10 +2,8 @@
 A PyTorch-powered python toolbox to train deep neural networks to perform motor tasks.
 """
 
-from importlib import metadata
-
 __name__ = "motornet"
-__version__ = metadata.version("motornet")
+__version__ = "0.2.5"
 
 from . import muscle
 from . import skeleton
