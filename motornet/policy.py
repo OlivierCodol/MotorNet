85bd2152198436d07568e35f8601e725d513302c
JonathanAMichaels <Jonathan.asher.michaels@gmail.com>
2023-10-19 22:36:37 -0400

    Updates to policy: - Added optional delay between modules. This adds significant processing time. - Added trainable initial state - Added optional spectral scaling of recurrent weight matrix

--- motornet/policy.py
+++ motornet/policy.py
@@ -50,18 +50,27 @@ class ModularPolicyGRU(nn.Module):
                  vision_mask: list, proprio_mask: list, task_mask: list,
                  connectivity_mask: np.ndarray, output_mask: list,
                  vision_dim: list, proprio_dim: list, task_dim: list,
+                 connectivity_delay: np.ndarray, spectral_scaling=None,
                  device=th.device("cpu"), random_seed=None, activation='tanh'):
         super(ModularPolicyGRU, self).__init__()
 
         # Store class info
         hidden_size = sum(module_size)
-        self.activation = activation
+        assert activation == 'tanh' or activation == 'rect_tanh'
+        if activation == 'tanh':
+            self.activation = lambda hidden: th.tanh(hidden)
+        elif activation == 'rect_tanh':
+            self.activation = lambda hidden: th.max(th.zeros_like(hidden), th.tanh(hidden))
+        self.spectral_scaling = spectral_scaling
         self.device = device
         self.num_modules = len(module_size)
         self.input_size = input_size
         self.module_size = module_size
         self.hidden_size = hidden_size
         self.output_size = output_size
+        self.connectivity_delay = connectivity_delay
+        self.max_delay = np.max(connectivity_delay)
+        self.h_buffer = []
 
         # Set the random seed
         if random_seed:
@@ -78,6 +87,8 @@ class ModularPolicyGRU(nn.Module):
         assert len(vision_dim) + len(proprio_dim) + len(task_dim) == self.input_size
 
         # Initialize all GRU parameters
+        # Initial hidden state
+        self.h0 = nn.Parameter(th.zeros(1, hidden_size))
         # Update gate
         self.Wz = nn.Parameter(th.cat((nn.init.xavier_uniform_(th.Tensor(hidden_size, input_size), gain=1),
                                        nn.init.orthogonal_(th.Tensor(hidden_size, hidden_size))), dim=1))
@@ -91,11 +102,18 @@ class ModularPolicyGRU(nn.Module):
                                        nn.init.orthogonal_(th.Tensor(hidden_size, hidden_size))), dim=1))
         self.bh = nn.Parameter(th.zeros(hidden_size))
 
+        # Optional rescaling of Wh eigenvalues
+        if self.spectral_scaling:
+            Wh_i, Wh = th.split(self.Wh.detach(), [input_size, hidden_size], dim=1)
+            eig = th.abs(th.linalg.eigvals(Wh))
+            Wh = self.spectral_scaling * (Wh / eig[0])
+            self.Wh = nn.Parameter(th.cat((Wh_i, Wh), dim=1))
+
         # Initialize all output parameters
         self.Y = nn.Parameter(nn.init.xavier_uniform_(th.Tensor(output_size, hidden_size), gain=1))
-        self.bY = nn.Parameter(nn.init.constant_(th.Tensor(output_size), -4.))
+        self.bY = nn.Parameter(nn.init.constant_(th.Tensor(output_size), -5.))
 
-        # create indices for indexing modules
+        # Create indices for indexing modules
         module_dims = []
         for m in range(self.num_modules):
             if m > 0:
@@ -181,16 +199,46 @@ class ModularPolicyGRU(nn.Module):
         self.to(device)
 
     def forward(self, x, h_prev):
-        # TODO: Add time delays between neural modules. This would be possible by looping through an h_prev time
-        #  buffer and then concatenating h together from the results of different time delays
-        concat = th.cat((x, h_prev), dim=1)
-        z = th.sigmoid(F.linear(concat, self.Wz, self.bz))
-        r = th.sigmoid(F.linear(concat, self.Wr, self.br))
-        concat_hidden = th.cat((x, r * h_prev), dim=1)
-        h_tilda = th.tanh(F.linear(concat_hidden, self.Wh, self.bh))
-        if self.activation == 'rect_tanh':
-            h_tilda = th.max(th.zeros_like(h_tilda), h_tilda)
-        h = (1 - z) * h_prev + z * h_tilda
-        y = th.sigmoid(F.linear(h, self.Y, self.bY))
+        # If there are delays between modules we need to go module-by-module (this is slow)
+        if self.max_delay > 0:
+            # Update hidden state buffer
+            self.h_buffer[:, :, 1:] = self.h_buffer[:, :, 0:-1]
+            self.h_buffer[:, :, 0] = h_prev
+            # Forward pass
+            h_new = th.zeros_like(h_prev)
+            for i in range(self.num_modules):
+                # Prepare delayed hidden states for each module
+                h_prev_delayed = th.zeros_like(h_prev)
+                for j in range(self.num_modules):
+                    h_prev_delayed[:, self.module_dims[j]] = self.h_buffer[:, self.module_dims[j],
+                                                                           self.connectivity_delay[i, j]]
+                concat = th.cat((x, h_prev_delayed), dim=1)
+                z = th.sigmoid(F.linear(concat, self.Wz[self.module_dims[i], :], self.bz[self.module_dims[i]]))
+                r = th.sigmoid(F.linear(concat, self.Wr, self.br))
+                concat_hidden = th.cat((x, r * h_prev_delayed), dim=1)
+                h_tilda = self.activation(F.linear(concat_hidden, self.Wh[self.module_dims[i], :],
+                                                   self.bh[self.module_dims[i]]))
+                h = (1 - z) * h_prev_delayed[:, self.module_dims[i]] + z * h_tilda
+                # Store new hidden states to correct module
+                h_new[:, self.module_dims[i]] = h
 
-        return y, h
+        # If there are no delays between modules we can do a single pass
+        else:
+            concat = th.cat((x, h_prev), dim=1)
+            z = th.sigmoid(F.linear(concat, self.Wz, self.bz))
+            r = th.sigmoid(F.linear(concat, self.Wr, self.br))
+            concat_hidden = th.cat((x, r * h_prev), dim=1)
+            h_tilda = self.activation(F.linear(concat_hidden, self.Wh, self.bh))
+            h_new = (1 - z) * h_prev + z * h_tilda
+
+        # Output layer
+        y = th.sigmoid(F.linear(h_new, self.Y, self.bY))
+        return y, h_new
+
+    def init_hidden(self, batch_size):
+        # Tile learnable hidden state
+        h0 = th.tile(self.activation(self.h0), (batch_size, 1))
+        # Create initial hidden state buffer if needed
+        if self.max_delay > 0:
+            self.h_buffer = th.tile(h0.unsqueeze(dim=2), (1, 1, self.max_delay+1))
+        return h0
