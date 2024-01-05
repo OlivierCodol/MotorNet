41178a10675fcf01208f8916eb629ce8a2b48b73
Olivier Codol <codol.olivier@gmail.com>
2024-01-04 20:24:51 -0500

    fix pathing

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..', '..', 'motornet')))
+sys.path.insert(0, os.path.abspath('..'))
 from motornet import __version__ as version
 
 # Workaround for issue https://github.com/sphinx-contrib/googleanalytics/issues/2
